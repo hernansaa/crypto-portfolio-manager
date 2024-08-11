11a7832639e254d4b28a1ba4f0d0da55164bda1b
hernansaa <hernansaa88@gmail.com>
2024-08-11 17:43:32 +0200

    add super user

--- core/settings.py
+++ core/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'market_data',
+    'portfolios',
+    'api',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
