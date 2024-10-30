864b422a4c1dea56fe0abdd2ff899c958660cf99
hernansaa <hernansaa88@gmail.com>
2024-10-30 02:21:34 +0100

    install newrelic

--- core/settings.py
+++ core/settings.py
@@ -169,14 +169,18 @@ else:
     CSRF_TRUSTED_ORIGINS = []  # or set a default value
 
 DATABASES = {
-    "default": {
-        "ENGINE": os.environ.get("SQL_ENGINE"),
-        "NAME": os.environ.get("SQL_DATABASE"),
-        "USER": os.environ.get("SQL_USER"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD"),
-        "HOST": os.environ.get("SQL_HOST"),
-        "PORT": os.environ.get("SQL_PORT"),
-    }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    # "default": {
+    #     "ENGINE": os.environ.get("SQL_ENGINE"),
+    #     "NAME": os.environ.get("SQL_DATABASE"),
+    #     "USER": os.environ.get("SQL_USER"),
+    #     "PASSWORD": os.environ.get("SQL_PASSWORD"),
+    #     "HOST": os.environ.get("SQL_HOST"),
+    #     "PORT": os.environ.get("SQL_PORT"),
+    # }
 }
 
 
