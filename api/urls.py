545bcef96a70cbd4fbed2b6490bffddeb62d0b63
hernansaa <hernansaa88@gmail.com>
2024-10-30 23:17:49 +0100

    update nginx conf

--- api/urls.py
+++ api/urls.py
@@ -14,8 +14,9 @@ router.register(r'portfolio_transactions', views.PorfolioTransactionViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('api/api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('', views.api_root, name='api-root'),  # Map root URL "/" to api_root view
     path('api/', views.api_root),  # Custom API root view
+    path('api/api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/health/', views.health_check, name='health-check'),
     path('api/', include(router.urls)),
 ]
